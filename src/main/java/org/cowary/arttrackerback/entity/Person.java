a0fbdaa80ebd9629007f9e63f06dbb5af02f4080
cowary <airvary7@gmail.com>
2025-11-30 14:23:27 +0300

    Add liquibase

--- src/main/java/org/cowary/arttrackerback/entity/Person.java
+++ src/main/java/org/cowary/arttrackerback/entity/Person.java
@@ -1,33 +1,33 @@
-package org.cowary.arttrackerback.entity;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@ToString
-
-@Entity(name = "person")
-public class Person {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String nameEn;
-    private String nameRu;
-    private int integrationId;
-
-    public Person(String nameEn, String nameRu, int integrationId) {
-        this.nameEn = nameEn;
-        this.nameRu = nameRu;
-        this.integrationId = integrationId;
-    }
-
-    public Person() {
-    }
-}
+//package org.cowary.arttrackerback.entity;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import lombok.Getter;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+//@Getter
+//@Setter
+//@ToString
+//
+//@Entity(name = "person")
+//public class Person {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private String nameEn;
+//    private String nameRu;
+//    private int integrationId;
+//
+//    public Person(String nameEn, String nameRu, int integrationId) {
+//        this.nameEn = nameEn;
+//        this.nameRu = nameRu;
+//        this.integrationId = integrationId;
+//    }
+//
+//    public Person() {
+//    }
+//}
--- src/main/java/org/cowary/arttrackerback/repo/PersonRepo.java
+++ src/main/java/org/cowary/arttrackerback/repo/PersonRepo.java
@@ -1,11 +1,11 @@
-package org.cowary.arttrackerback.repo;
-
-import org.cowary.arttrackerback.entity.Person;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
-
-public interface PersonRepo extends CrudRepository<Person, Long> {
-
-    Optional<Person> findByNameEn(String nameEn);
-}
+//package org.cowary.arttrackerback.repo;
+//
+//import org.cowary.arttrackerback.entity.Person;
+//import org.springframework.data.repository.CrudRepository;
+//
+//import java.util.Optional;
+//
+//public interface PersonRepo extends CrudRepository<Person, Long> {
+//
+//    Optional<Person> findByNameEn(String nameEn);
+//}
